ac4cc970bb663b1fe4e97b6668d807eccfd3aeae
Ben Barrett <barrettb@pvipanther.net>
2017-07-10 19:32:33 -0400

    Android Project

---
+++ android/my-sample-game-android/assets/Castle.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Castle" tilewidth="32" tileheight="32" tilecount="256" columns="16">
+ <image source="Castle Tiles for RPG's _ OpenGameArt.org_files/Castle2_0(1).png" width="512" height="512"/>
+</tileset>
